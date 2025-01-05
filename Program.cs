4dc15512557d64f2b9f43fc41c795f1c56575ce5
Misaka-L <lipww1234@foxmail.com>
2025-01-05 22:44:31 +0800

    feat: response & request decoding and encoding

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.IO.Compression;
+using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
 using HtmlAgilityPack;
@@ -66,25 +68,58 @@ app.MapForwarder("{**catch-all}", forwarderOptions.Endpoint, context =>
         return ValueTask.CompletedTask;
     });
 
-    context.AddRequestHeaderRemove("Accept-Encoding");
+    context.AddRequestHeader("Accept-Encoding", "gzip, deflate, br", false);
 
     context.AddResponseTransform(async responseTransformContext =>
     {
-        if (responseTransformContext.ProxyResponse is not { } response)
+        if (responseTransformContext.ProxyResponse is null)
             return;
 
-        var originContent = response.Content;
+        var clientPreferEncodings = responseTransformContext.HttpContext.Request.Headers.AcceptEncoding.ToString()
+            .Trim().Replace(" ", "").Split(',');
+        var responseEncoding = clientPreferEncodings.Contains("br") ? "br" :
+            clientPreferEncodings.Contains("gzip") ? "gzip" :
+            clientPreferEncodings.Contains("deflate") ? "deflate" : "none";
+
+        var originContent = responseTransformContext.ProxyResponse.Content;
+        var response = responseTransformContext.HttpContext.Response;
+
+        response.Headers.Server = "1PanelAuth";
+        responseTransformContext.ProxyResponse.Headers.Server.Clear();
+        responseTransformContext.ProxyResponse.Headers.Server.Add(new ProductInfoHeaderValue("1PanelAuth", null));
+
         if (originContent.Headers.ContentType?.MediaType != "text/html")
         {
+            if (clientPreferEncodings.Contains(originContent.Headers.ContentEncoding.FirstOrDefault()))
+                return;
+
+            responseTransformContext.SuppressResponseBody = true;
+            await using var contentStream = await GetHttpContentDecodeStreamAsync(originContent);
+
+            response.ContentLength = null;
+
+            if (responseEncoding == "none")
+            {
+                response.Headers.Remove("Content-Encoding");
+                await contentStream.CopyToAsync(response.Body);
+            }
+
+            response.Headers.ContentEncoding = responseEncoding;
+            await using var compressStream = GetCompressStream(response.Body, responseEncoding);
+
+            await contentStream.CopyToAsync(compressStream);
+
             return;
         }
 
-        if (originContent.Headers.ContentEncoding.Count != 0)
+        if (!originContent.Headers.ContentEncoding.Any(IsSupportedEncoding))
         {
             return;
         }
 
-        var rawHtml = await originContent.ReadAsStringAsync();
+        var rawHtml = await DecodingHttpContentAsync(originContent);
+
+        responseTransformContext.SuppressResponseBody = true;
 
         var htmlDoc = new HtmlDocument();
         htmlDoc.LoadHtml(rawHtml);
@@ -122,11 +157,79 @@ app.MapForwarder("{**catch-all}", forwarderOptions.Endpoint, context =>
 
         htmlDoc.DocumentNode.SelectSingleNode("//body")?.AppendChild(scriptElement);
 
-        response.Content = new StringContent(htmlDoc.DocumentNode.OuterHtml, Encoding.UTF8, "text/html");
+        var responseHtml = htmlDoc.DocumentNode.OuterHtml;
+
+        if (!clientPreferEncodings.Any(IsSupportedEncoding))
+        {
+            response.Headers.Remove("Content-Encoding");
+            response.ContentLength = null;
+
+            await using var streamWriter = new StreamWriter(response.Body);
+            await streamWriter.WriteAsync(responseHtml);
+
+            return;
+        }
+
+        await using var responseContentStream = new MemoryStream();
+        await using var responseContentStreamWriter = new StreamWriter(responseContentStream);
+
+        await responseContentStreamWriter.WriteAsync(responseHtml);
+        responseContentStream.Seek(0, SeekOrigin.Begin);
+
+        response.Headers.ContentEncoding = responseEncoding;
+        await using var compressResponseStream = GetCompressStream(response.Body, responseEncoding);
+
+        await responseContentStream.CopyToAsync(compressResponseStream);
     });
 });
 
 app.Run();
+
+async ValueTask<string> DecodingHttpContentAsync(HttpContent httpContent)
+{
+    await using var decodeStream = await GetHttpContentDecodeStreamAsync(httpContent);
+    using var streamReader = new StreamReader(decodeStream);
+
+    return await streamReader.ReadToEndAsync();
+}
+
+async ValueTask<Stream> GetHttpContentDecodeStreamAsync(HttpContent httpContent)
+{
+    var contentStream = await httpContent.ReadAsStreamAsync();
+    if (httpContent.Headers.ContentEncoding.FirstOrDefault() is not { } encoding)
+    {
+        return contentStream;
+    }
+
+    return GetDecompressStream(contentStream, encoding);
+}
+
+Stream GetDecompressStream(Stream contentStream, string encoding)
+{
+    return encoding switch
+    {
+        "gzip" => new GZipStream(contentStream, CompressionMode.Decompress),
+        "deflate" => new DeflateStream(contentStream, CompressionMode.Decompress),
+        "br" => new BrotliStream(contentStream, CompressionMode.Decompress),
+        _ => contentStream
+    };
+}
+
+Stream GetCompressStream(Stream outputStream, string encoding)
+{
+    return encoding switch
+    {
+        "gzip" => new GZipStream(outputStream, CompressionLevel.Optimal),
+        "deflate" => new DeflateStream(outputStream, CompressionLevel.Optimal),
+        "br" => new BrotliStream(outputStream, CompressionLevel.Optimal),
+        _ => throw new NotSupportedException("The encoding is not supported.")
+    };
+}
+
+bool IsSupportedEncoding(string encoding)
+{
+    return encoding is "gzip" or "deflate" or "br";
+}
 
 internal class ForwarderOptions
 {
